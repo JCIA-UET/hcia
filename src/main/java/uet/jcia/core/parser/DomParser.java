653d905b2b97e4b32e4471125097d004fcd190b7
hieusonson9x <hieusonson9x@gmail.com>
2016-07-11 23:09:13 +0700

    no change

--- src/main/java/uet/jcia/core/parser/DomParser.java
+++ src/main/java/uet/jcia/core/parser/DomParser.java
@@ -1,5 +1,10 @@
 package uet.jcia.core.parser;
 
-public class DomParser {
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.w3c.dom.Document;
+
+public class DomParser {
+	
 }
